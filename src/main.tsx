87f4549b10904ab6efde9f887d6f8b86c7f53a34
julianfere_ <julianferegotti96@gmail.com>
2022-12-29 20:28:12 -0300

    added react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Home from "./pages/home";
 import "./style.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Config } from "./pages/config";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/config",
+    element: <Config />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
