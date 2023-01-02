54f7d87576a39f153e0dc6176903f2898ce38300
Julian Feregotti <julian.feregotti@snappler.com>
2023-01-02 15:40:36 -0300

    using grid

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { SideBar } from "../../components/sidebar";
+import "./index.scss";
 
 export const Template = () => {
   return (
-    <>
+    <main className="parent">
       <SideBar />
-      <Outlet />
-    </>
+      <section className="content">
+        <Outlet />
+      </section>
+    </main>
   );
 };
